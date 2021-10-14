90c404259ca8ab3bce6095b6195fbd765ed7f427
De Wet Oosthuizen <dewetharm@gmail.com>
2021-10-14 20:09:03 +0100

    Assignment questions updated

--- TEST/Program.cs
+++ TEST/Program.cs
@@ -6,7 +6,32 @@ namespace TEST
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Ask user for their name
+            Console.WriteLine("Enter username");
+
+            // Create a string variable and store userName provided in the variable
+            string userName = Console.ReadLine();
+        
+            // Display Hello message with userName
+            Console.WriteLine("Hello " + userName);
+
+            // Ask the user a question
+            Console.WriteLine("Are you having a good day? (Y/N)");
+
+            // Display response depending on their answer
+            ConsoleKeyInfo cki = Console.ReadKey();
+            if (cki.Key.ToString() == "Y")
+            {
+                Console.WriteLine("\nThat is great! Hope your day continues like this");
+            }
+            else if (cki.Key.ToString() == "N")
+            {
+                Console.WriteLine("\nThat is a shame. Hope your day gets better from here");
+            }
+            else
+            {
+                Console.WriteLine("\nPlease ensure to select either Y or N");
+            }
         }
     }
 }
